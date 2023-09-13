0fe789f749fd1e007baa09876da8547dec062f71
shikhar311993 <68625780+shikhar311993@users.noreply.github.com>
2023-09-12 21:01:28 -0400

    Update badge4_data_lake_cata.py

--- badge4_data_lake_cata.py
+++ badge4_data_lake_cata.py
@@ -4,4 +4,4 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
-streamlit.header("View our Fruit List - Add your Favorites!")
+streamlit.header("Zena\'s Amazing Athleisure Catalog")
